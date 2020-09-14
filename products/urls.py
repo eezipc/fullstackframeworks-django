f8cc7a1d7b957f56985f0ca525469cb7c2779da9
Alan Staunton <eezipc@gmail.com>
2020-09-14 20:37:48 +0000

    added product details page

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.allshopitems, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
     
 ]
--- products/views.py
+++ products/views.py
@@ -14,3 +14,16 @@ def allshopitems(request):
 
 
     return render(request, 'products/shopping.html', context)
+
+
+def product_detail(request, product_id):
+    """ Show single items in the shop """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product' : product,
+    }
+
+
+    return render(request, 'products/shopping_detail.html', context)
