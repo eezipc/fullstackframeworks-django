1d164068d79c4b500608667d87ab8c1d20a54a68
Alan Staunton <eezipc@gmail.com>
2020-09-24 12:57:26 +0000

    added simple stripe. Payment successful

--- checkout/signals.py
+++ checkout/signals.py
@@ -5,8 +5,10 @@ from .models import SingleOrder
 
 @receiver(post_save, sender=SingleOrder)
 def updateonsave(sender, instance, created, **kwargs):
+# Line below may be incorrect. Check later. Userorders may be orders (or something else)
     instance.Userorders.updateorder()
 
 @receiver(post_delete, sender=SingleOrder)
 def deleteonsave(sender, instance, **kwargs):
+# Line below may be incorrect. Check later. Userorders may be orders (or something else)
     instance.Userorders.updateorder()
--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,52 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
+from basket.orders import basketorders
+
+import stripe
 
 # Create your views here.
 
 def checkout(request):
+
+    stripepublickey = settings.STRIPEPUBLICKEY
+    stripesecretkey = settings.STRIPESECRETKEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "The basket is empty")
         return redirect(reverse('products'))
 
+    stripebasket = basketorders(request)
+    stripetotal = stripebasket['finaltotal']
+    finalstripetotal = round(stripetotal * 100)
+    stripe.api_key = stripesecretkey
+    intent = stripe.PaymentIntent.create(
+        amount=finalstripetotal,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+# Remove this when finished project
+
+    print(intent)
+
+
+
     order_form = OrderForm()
+
+    if not stripepublickey:
+        messages.warning(request, 'Stripe public key is missing')
+
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripepublickey' : stripepublickey,
+        'client_secret': intent.client_secret,
+
+ 
     }
 
     return render(request, template, context)
