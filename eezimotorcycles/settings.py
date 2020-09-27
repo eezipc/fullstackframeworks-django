561bbab9e5dccac2528a52c7f9889b9453990a97
Alan Staunton <eezipc@gmail.com>
2020-09-27 18:29:56 +0000

    added profile form

--- eezimotorcycles/settings.py
+++ eezimotorcycles/settings.py
@@ -109,7 +109,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/profile'
+LOGIN_REDIRECT_URL = '/userprofiles'
 
 WSGI_APPLICATION = 'eezimotorcycles.wsgi.application'
 
---
+++ userprofiles/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',
+                  )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'userphone': 'Phone Number',
+            'useraddress1': 'Address',
+            'useraddress2': 'Address',
+            'usertown': 'Town',
+            'usercounty': 'County',
+            'userpostalcode': 'Postal Code',
+            'usercountry': 'Country',
+                        
+        }
+
+        self.fields['userphone'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = ''
+            self.fields[field].label = False
