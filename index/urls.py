a6d8bbe7f737c6b6d6db9472884034bb98c9a10e
Alan Staunton <eezipc@gmail.com>
2020-10-11 17:48:06 +0000

    added contact form and modal popup

--- index/urls.py
+++ index/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
     
 ]
--- index/views.py
+++ index/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 """ Show the index page """
 def index(request):
     return render(request, 'index/index.html')
+
+  
+""" Show the contact page """
+def contact(request):
+    return render(request, 'index/contact.html')
